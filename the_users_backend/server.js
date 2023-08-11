930081e818a2819c97d52ae9fad451765a3e8087
OreoluwaSolaOjo <oreoluwasolaojo@gmail.com>
2023-08-11 08:17:49 +0100

    new commm

--- the_users_backend/server.js
+++ the_users_backend/server.js
@@ -3,7 +3,11 @@ const userRoutes = require('./userRoutes');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://64d5dd4dc55b45363f28912e--earnest-strudel-469a22.netlify.app'
+}));
+
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/uploads', express.static('uploads'))
