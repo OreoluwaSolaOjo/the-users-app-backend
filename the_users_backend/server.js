28ce3741f696675bc7d752c77c14706e03f7283c
OreoluwaSolaOjo <oreoluwasolaojo@gmail.com>
2023-08-11 11:11:15 +0100

    Changess

--- the_users_backend/server.js
+++ the_users_backend/server.js
@@ -1,6 +1,9 @@
+
+const cors = require('cors');
+app.use(cors())
 const express = require('express');
 const userRoutes = require('./userRoutes');
-const cors = require('cors');
+
 const app = express();
 
 // app.use(cors());
@@ -11,22 +14,22 @@ const app = express();
 //   origin: 'https://64d5ff21a3736a46e03551eb--earnest-strudel-469a22.netlify.app',
 //   credentials: true
 // }));
-const allowedOrigins = [
-  'https://64d5ff21a3736a46e03551eb--earnest-strudel-469a22.netlify.app',
-  'https://64d5dcd0ff6c0b35036ba6ab--earnest-strudel-469a22.netlify.app',
-  //... you can add other allowed URLs here
-];
+// const allowedOrigins = [
+//   'https://64d5ff21a3736a46e03551eb--earnest-strudel-469a22.netlify.app',
+//   'https://64d5dcd0ff6c0b35036ba6ab--earnest-strudel-469a22.netlify.app',
+//   //... you can add other allowed URLs here
+// ];
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    credentials: true
-}));
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     },
+//     credentials: true
+// }));
 
 app.use(express.json());
 app.use('/uploads', express.static('uploads'))
